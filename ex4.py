24d9c8d2a53372b53adb29712372aead27d0a8a4
Teodora Uksanovic <teodora.uksanovic@udg.edu.me>
2018-03-08 11:51:08 +0100

    ex4

---
+++ ex4.py
@@ -0,0 +1,21 @@
+def suma_cifara(broj):
+
+    suma = 0
+    for i in str(broj):
+        cifra = int(i)
+        suma = suma + cifra
+        print(cifra)
+    return suma
+
+
+def suma_cifara_matematicari(broj):
+    suma=0
+    while (broj > 0):
+        cifra = broj % 10
+        broj = broj // 10
+        suma = suma+cifra
+        print(cifra)
+
+
+print("----------")
+print(suma_cifara_matematicari(1546821))
