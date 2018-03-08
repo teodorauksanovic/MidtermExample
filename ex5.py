4182d8cf59f474632737ecbebe849f018c8dd4cb
Teodora Uksanovic <teodora.uksanovic@udg.edu.me>
2018-03-08 11:58:36 +0100

    ex5

---
+++ ex5.py
@@ -0,0 +1,15 @@
+broj_elemenata_niza = int(input("Unesite broj elemenata niza: "))
+
+niz = []
+parni = 0
+neparni = 0
+
+for i in range (broj_elemenata_niza):
+
+    element_niza = input("Unesite novi element: ")
+    niz.append(element_niza)
+
+
+print(niz)
+print(parni)
+print(neparni)
